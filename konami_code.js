ad944c3750df4d818623016f88f58abbbf451567
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-11 12:30:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,20 +3,10 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
-    const key = parseInt(e.detail || e.which);
-
-    if (key === code[index]) {
-      index++;
-
-      if (index === code.length) {
-        alert("Hurray!");
-
-        index = 0;
-      }
-    } else {
-      index = 0;
-    }
-  }
+  for (let x = 0; x < code.length; x++) {
+    var body = document.querySelector('body')
+    if (code[x] === 
+}
 }
 
 init();
