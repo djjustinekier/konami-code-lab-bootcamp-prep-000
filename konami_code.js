854ad89f35b91ce04c661cf3aeea9f22f0e87c40
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-11 13:49:42 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let codePos = 0;
 document.addEventListener('keydown', function (e) {
   var keyEquiv = parseInt(e.which || e.detail);
-  var keyCode = code(codePos);
+  var keyCode = code[codePos];
   if (keyCode === keyEquiv) {
     codePos++;
     if (codePos === code.length) {
